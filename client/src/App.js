d180f3fc1b002e665defaaee025f96889aee654a
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-22 13:46:44 +0600

    Ready to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Launch from './components/Launch';
 
 const cache = new InMemoryCache();
 const link = createHttpLink({
-  uri: 'http://localhost:5000/graphql'
+  uri: '/graphql'
 });
 
 const client = new ApolloClient({
---
+++ public/precache-manifest.8ba422a79c4fc2f8a13508c2e3ed10c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a74d04f8711380641a8d7eb70f1bd2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eec0c11c8ecd9417ea36",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0db685d5811f7e9f3592",
+    "url": "/static/js/2.4574d8b3.chunk.js"
+  },
+  {
+    "revision": "1168987de8c022b65d0b713d94cc1242",
+    "url": "/static/js/2.4574d8b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec0c11c8ecd9417ea36",
+    "url": "/static/js/main.6cfec1dc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "481e9cbb5dc4c4083ac62819a0d20e88",
+    "url": "/static/media/logo.481e9cbb.png"
+  }
+]);
